f57fbd7fd1680ae784081a5ad637a664032c7c47
Isaac Ayodele <ziggycodes25@gmail.com>
2022-12-18 12:13:29 +0100

    python practice

---
+++ conditions.py
@@ -0,0 +1,8 @@
+n = input("Number: ")
+
+if int(n) > 0:
+    print("n is positive")
+elif int(n) < 0:
+    print("n is negative")
+else:
+    print("n equals zero")
---
+++ sequences.py
@@ -0,0 +1,7 @@
+Potter = ["Harry", "Ron", "Hermione"]
+
+print(Potter)
+Potter.append ("Draco")
+Potter.sort()
+print("Draco has been added to the list")
+print(Potter)
